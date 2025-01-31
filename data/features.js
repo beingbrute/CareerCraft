a4c192661185f437532244e3a66fd40f5cec5b71
AdityaRanjan <adityaranjan17302215@gmail.com>
2025-01-31 17:02:42 +0530

    preparing for deployment

---
+++ data/features.js
@@ -0,0 +1,28 @@
+import { BrainCircuit, Briefcase, LineChart, ScrollText } from "lucide-react";
+
+export const features = [
+  {
+    icon: <BrainCircuit className="w-10 h-10 mb-4 text-primary" />,
+    title: "AI-Powered Career Guidance",
+    description:
+      "Receive personalized career advice and insights driven by advanced AI technology.",
+  },
+  {
+    icon: <Briefcase className="w-10 h-10 mb-4 text-primary" />,
+    title: "Interview Preparation",
+    description:
+      "Practice with job-specific questions and receive instant feedback to enhance your performance.",
+  },
+  {
+    icon: <LineChart className="w-10 h-10 mb-4 text-primary" />,
+    title: "Industry Insights",
+    description:
+      "Stay ahead with up-to-the-minute industry trends, salary insights, and in-depth market analysis.",
+  },
+  {
+    icon: <ScrollText className="w-10 h-10 mb-4 text-primary" />,
+    title: "Smart Resume Creation",
+    description:
+      "Create AI-powered, ATS-optimized resumes that stand out to employers.",
+  },
+];
